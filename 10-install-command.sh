c24e429d03c9e6ffa78f40dc0f98aae04e886f27
Nandu-Addanki1999 <nandua1999@gmail.com>
2025-05-21 23:57:14 +0530

    hello world started

---
+++ 10-install-command.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+USER=$(id -u)
+
+if [ $USERID -ne 0 ]
+then  
+    echo "ERROR:: Please run this script with root access"
+else  
+    echo "You are running with root access"
+fi
+dnf install mysql -y
